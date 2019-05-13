8b5cdbe8c466bfb57133d4d644c2c5748d413982
Heidi Hoopes <heidih@gmail.com>
2019-05-12 18:52:35 -0600

    Update clean.sh

--- bin/clean.sh
+++ bin/clean.sh
@@ -2,9 +2,11 @@
 
 timestamp=$( date +%T )
 
-mv ../bot.js ../other_projects/${timestamp}_bot.js
+mv bot.js other_projects/${timestamp}_bot.js
 
 git reset --hard origin/master
 git init
 git remote add origin https://github.com/hhoopes/GESTEM.git
 git pull
+
+refresh
