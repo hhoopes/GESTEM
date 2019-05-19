f9700e9bac7e892ea7083e177a18afb47b7a09f7
Heidi Hoopes <heidih@gmail.com>
2019-05-19 10:03:18 -0600

    Check bot's id before responding

--- bot.js
+++ bot.js
@@ -6,39 +6,41 @@ bot.on('ready', () => {                                                  // When
     console.log("Yeehaw! I'm awake!");
 });
  
-bot.on("messageCreate", (message) => { // When a message is created
-    var msg = message.content.toLowerCase();                             // We don't want our user having to worry about capitalization
-    console.log("The message is:", msg);
+bot.on("messageCreate", (message) => {                                   // When a message is created
+    if(message.user.id != bot.id) {                                      // Our bot shouldn't respond to its own messages
+        var msg = message.content.toLowerCase();                         // We don't want our user having to worry about capitalization
+        console.log("The message is:", msg);
     
-  if(msg.includes("hello")) {
-      bot.createMessage(message.channel.id,
-        "world!"
-      )
-    
-  } else if(msg.includes("what transportation options are available")) { // Look for the word pattern that starts one of our user's questions
-    bot.createMessage(message.channel.id,
-      // Add your response in the "" marks
-      ""
-    )
+        if(msg.includes("hello")) {                                     // Look for the message "hello"
+            bot.createMessage(message.channel.id,   
+                "world!"
+            )
+        
+        } else if(msg.includes("Question 1!")) {                        // Look for the word pattern that starts one of our user's questions
+            bot.createMessage(message.channel.id,
+                // Add your response in the "" marks
+                ""
+            )
       
-  } else if(msg.includes("what locations are popular")) {               // look for the pattern for another of our user's questions
-      bot.createMessage(message.channel.id,
-        // Add your response in "" marks
-        ""
-    );
+        } else if(msg.includes("Question 2!")) {                        // look for the pattern for another of our user's questions
+            bot.createMessage(message.channel.id,
+                // Add your response in "" marks
+                ""
+            )
       
-  } else if(msg.includes("what locations are popular")) {               // look for the pattern for another of our user's questions
-      bot.createMessage(message.channel.id,
-        // Add your response in "" marks
-        ""
-    );
+        } else if(msg.includes("Question 3!")) {                        // look for the pattern for another of our user's questions
+            bot.createMessage(message.channel.id,
+                // Add your response in "" marks
+                ""
+            );
       
-  } else if(msg.includes("Add your own message you want the bot to look for here!")) {
-      bot.createMessage(message.channel.id,
-        // Add your response in "" marks
-        ""
-    );
-  }
+        } else if(msg.includes("")) {
+            bot.createMessage(message.channel.id,
+                // Add your response in "" marks
+                ""
+            );
+        }
+    }
 });
 
 
