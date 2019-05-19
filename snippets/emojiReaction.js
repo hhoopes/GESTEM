1a3420fefe6d5c4dd95589e30ae085931b185aeb
Heidi Hoopes <heidih@gmail.com>
2019-05-19 11:57:10 -0600

    Create emojiReaction.js

---
+++ snippets/emojiReaction.js
@@ -0,0 +1,4 @@
+// copy line #4 to add a reaction automatically once the bot reads a message. You can use any of the emoji!
+// emoji are listed at this URL: https://unicode.org/emoji/charts/full-emoji-list.html#1f604
+
+addReaction("U+1F923", message.author.id)
