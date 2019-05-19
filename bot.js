676e62c48b634842ec9ea907c3afb57683c71d8e
Heidi Hoopes <heidih@gmail.com>
2019-05-19 10:22:39 -0600

    Clean up code & don't respond to bot's own messages

--- bot.js
+++ bot.js
@@ -6,42 +6,44 @@ bot.on('ready', () => {                                                  // When
     console.log("Yeehaw! I'm awake!");
 });
  
-bot.on("messageCreate", (message) => {                                   // When a message is created
-    if(message.user.id != bot.id) {                                      // Our bot shouldn't respond to its own messages
-        var msg = message.content.toLowerCase();                         // We don't want our user having to worry about capitalization
-        console.log("The message is:", msg);
+bot.on("messageCreate", (message) => {                                 // When a message is created
+  if(message.author.id != bot.user.id) {                              // Our bot shouldn't respond to its own messages
+    var msg = message.content.toLowerCase();                         // We don't want to worry about capitalization
+    console.log("The message is:", msg);
     
-        if(msg.includes("hello")) {                                     // Look for the message "hello"
-            bot.createMessage(message.channel.id,   
-                "world!"
-            )
-        
-        } else if(msg.includes("Question 1!")) {                        // Look for the word pattern that starts one of our user's questions
-            bot.createMessage(message.channel.id,
-                // Add your response in the "" marks
-                ""
-            )
+    // Look for the message "hello"
+    if(msg.includes("hello")) {                                     
+      bot.createMessage(message.channel.id,   
+        "world!"
+      )
       
-        } else if(msg.includes("Question 2!")) {                        // look for the pattern for another of our user's questions
-            bot.createMessage(message.channel.id,
-                // Add your response in "" marks
-                ""
-            )
+    // Look for the word pattern that starts one of our user's questions
+    } else if(msg.includes("Question 1!")) {
+      bot.createMessage(message.channel.id,
+        // Add your response in the "" marks
+        ""
+      )
       
-        } else if(msg.includes("Question 3!")) {                        // look for the pattern for another of our user's questions
-            bot.createMessage(message.channel.id,
-                // Add your response in "" marks
-                ""
-            );
+    } else if(msg.includes("Question 2!")) {
+      bot.createMessage(message.channel.id,
+        // Add your response in the "" marks
+        ""
+      )
       
-        } else if(msg.includes("")) {
-            bot.createMessage(message.channel.id,
-                // Add your response in "" marks
-                ""
-            );
-        }
+    } else if(msg.includes("Question 3!")) {                        
+      bot.createMessage(message.channel.id,
+        // Add your response in the "" marks
+        ""
+      )
+      
+      } else if(msg.includes("")) {
+      bot.createMessage(message.channel.id,
+        // Add your response in the "" marks
+        ""
+      )
     }
-});
+  }
+})
 
-
-bot.connect();                                         // Get the bot to connect to Discord
+// Get the bot to connect to Discord
+bot.connect();                                         
