14c09008794a1317512f4f33efc5ddbefbae5d24
Heidi Hoopes <heidih@gmail.com>
2019-05-19 12:22:10 -0600

    Update embed.js

--- snippets/embed.js
+++ snippets/embed.js
@@ -1,26 +1,27 @@
-bot.createMessage(msg.channel.id, {
-            embed: {
-                title: "I'm an embed!", // Title of the embed
-                description: "Here is some more info, with **awesome** formatting.\nPretty *neat*, huh?",
-                author: { // Author property
-                    name: msg.author.username,
-                    icon_url: msg.author.avatarURL
-                },
-                color: 0x008000, // Color, either in hex (show), or a base-10 integer
-                fields: [ // Array of field objects
-                    {
-                        name: "Some extra info.", // Field title
-                        value: "Some extra value.", // Field
-                        inline: true // Whether you want multiple fields in same line
-                    },
-                    {
-                        name: "Some more extra info.",
-                        value: "Another extra value.",
-                        inline: true
-                    }
-                ],
-                footer: { // Footer text
-                    text: "Created with Eris."
-                }
+      bot.createMessage(message.channel.id, {
+        embed: {
+          title: "I have an answer for your question!", // Title of the embed
+          description: "Here is some more info, with **awesome** formatting.\nPretty *neat*, huh?",
+          author: {  // addresses the customer that had the question
+            name: message.author.username,
+            icon_url: message.author.avatarURL
+          },
+          color: 0xcde4e6, // Color of the bar on the side, you can find these numbers at https://www.color-hex.com/
+          fields: [ // Little text boxes that will go across the screen
+            {
+              name: "Some extra info.", // Field title
+              value: "Some extra value.", // Field
+              inline: true // Whether you want multiple boxes in same line
+            },
+            {
+              name: "Some more extra info.",
+              value: "Another extra value.",
+              inline: true
             }
-        })
+          ],
+          footer: { // Footer text
+            text: "GESTEM Intergalactic Travel Agency"
+          }
+            }
+        });
+  
