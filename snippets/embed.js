5a1049ab66f142b7aac9698fafd65eaca6c501c7
Heidi Hoopes <heidih@gmail.com>
2019-05-19 14:08:21 -0600

    Update embed.js

--- snippets/embed.js
+++ snippets/embed.js
@@ -1,27 +1,26 @@
-      bot.createMessage(message.channel.id, {
-        embed: {
-          title: "I have an answer for your question!", // Title of the embed
-          description: "Here is some more info, with **awesome** formatting.\nPretty *neat*, huh?",
-          author: {  // addresses the customer that had the question
-            name: message.author.username,
-            icon_url: message.author.avatarURL
-          },
-          color: 0xcde4e6, // Color of the bar on the side, you can find these numbers at https://www.color-hex.com/
-          fields: [ // Little text boxes that will go across the screen
-            {
-              name: "Some extra info.", // Field title
-              value: "Some extra value.", // Field
-              inline: true // Whether you want multiple boxes in same line
-            },
-            {
-              name: "Some more extra info.",
-              value: "Another extra value.",
-              inline: true
-            }
-          ],
-          footer: { // Footer text
-            text: "GESTEM Intergalactic Travel Agency"
-          }
-            }
-        });
-  
+bot.createMessage(message.channel.id, {
+  embed: {
+    title: "I have an answer for your question!", // Title of the embed
+    description: "Here is some more info, with **awesome** formatting.\nPretty *neat*, huh?",
+    author: {  // addresses the customer that had the question
+      name: message.author.username,
+      icon_url: message.author.avatarURL
+    },
+    color: 0xcde4e6, // Color of the bar on the side, you can find these numbers at https://www.color-hex.com/
+    fields: [ // Little text boxes that will go across the screen
+      {
+        name: "Some extra info.", // Field title
+        value: "Some extra value.", // Field
+        inline: true // Whether you want multiple boxes in same line
+      },
+      {
+        name: "Some more extra info.",
+        value: "Another extra value.",
+        inline: true
+      }
+    ],
+    footer: { // Footer text (copyright, signature, contact info, that kind of thing!)
+      text: "GESTEM Intergalactic Travel Agency"
+    }
+  }
+});
