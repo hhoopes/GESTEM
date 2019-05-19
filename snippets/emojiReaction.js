74db7ca73b5c71048422f4ea0b50e4123e05d32d
Heidi Hoopes <heidih@gmail.com>
2019-05-19 17:56:27 -0600

    Update emojiReaction.js

--- snippets/emojiReaction.js
+++ snippets/emojiReaction.js
@@ -1,4 +1,4 @@
-// copy line #4 to add a reaction automatically once the bot reads a message. You can use any of the emoji!
+// copy line #4 to add a reaction automatically once the bot reads a message. You can use any of the emoji in the browser column!
 // emoji are listed at this URL: https://unicode.org/emoji/charts/full-emoji-list.html#1f604
 
 message.channel.addMessageReaction(message.id, "😀")
